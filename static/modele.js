5fda226e2a7afc0216facf81aca7d7b83645509b
KawtarBahri <144914063+KawtarBahri@users.noreply.github.com>
2024-01-17 09:29:23 +0100

    Delete static/modele.js

--- static/modele.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const monSchema = mongoose.Schema({
-  Nom: { type: String, required: false }, //le nom est un champs non obligatoire
-  Recommandation: { type: String, required: true },  
-  Statut: { type: String, default: 'en attente' },
-});
-
-module.exports = mongoose.model('Recommandations', monSchema);
